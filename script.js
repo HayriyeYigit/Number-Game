494fd4bdf0a60309f75ec8f9f86b7ead2a789ee2
HayriyeYigit <hayriye.yigit@dci-student.org>
2024-10-17 10:33:03 +0200

    fertig

---
+++ script.js
@@ -0,0 +1,24 @@
+let numberToGuess = Math.round(Math.random() * 100);
+
+let tries = 0;
+
+function guessTheNumber() {
+  tries = tries + 1;
+  displayTries.innerHTML = "Versuche:" + tries;
+
+  if (numberToGuess == myNumber.value) {
+    headline.innerHTML = "Du hast gewonnen!";
+    let jsConfetti = new JSConfetti();
+
+jsConfetti.addConfetti();
+  }
+
+  if (numberToGuess > myNumber.value) {
+    headline.innerHTML = "Die Zahl ist größer!";
+  }
+
+  if (numberToGuess < myNumber.value) {
+    headline.innerHTML = "Die Zahl ist kleiner!";
+  }
+  myNumber.value = "";
+}
